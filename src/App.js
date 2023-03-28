e6fc552d8dc3433c6f8ea84e563e01e9a3c6e2ec
ItsFaizan <faizanjavid71@gmail.com>
2023-03-28 09:28:47 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import React from 'react';
 import FirstComponent from './components/FirstComponent'
-function App() {
-  return (
-    <div>
-    <h1> Hi Faizan</h1>
-   <FirstComponent/>
-    </div>
-  );
-}
+import { useState } from "react";
 
+const App = () => {
+ const [open, setOpen] = useState(false);
+
+ return (
+  <div className="App">
+   <button onClick={() => setOpen((prev) => !prev)}>toggle</button>
+   <div className={open ? "open" : "close"}>
+    I'm {open ? "opened" : "closed"}
+   </div>
+  </div>
+ );
+};
 export default App;
